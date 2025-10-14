14a87bd4c62c1ee3d21191f1726c05ac869f9061
tomlyle <tom.b.lyle@gmail.com>
2025-10-14 21:04:28 +1100

    Updated interface to be able to run seperately from the robot. Also, modified the readme and removed the pyproject.toml file - genuinely more hassle than expected

--- README.md
+++ README.md
@@ -3,16 +3,10 @@ Integrating a Large Language Model (LLM) with a SO-ARM 101 teleoperation unit fr
 
 ## Install Packages
 ### Python packages
-The main packages required from Python are LeRobot, ffmpeg and PyTorch. I recommend creating a virtual environment for installing the packages. Not needed, but definitely recommended.
+It is recommended you set up a virtual environment, however you prefer to do that. 
+Once done, install the below package.
 
 ```
-python -m venv .venv && source .venv/bin/activate
+python -m pip install lerobot
 ```
 
-Then just run the following:
-
-```
-pip install -e .                # editable install
-```
-
-For a full list of packages, check the `pyproject.toml` file.
--- Scripts/submodules/WebInterface/interface.py
+++ Scripts/submodules/WebInterface/interface.py
@@ -1,8 +1,29 @@
 from flask import Flask
+import logging
+
 
 def create_app(onUpdate = None, current_positions = None):
     app = Flask(__name__)
-    from .routes import register_routes
+    
+    from routes import register_routes
     register_routes(app, onUpdate, current_positions)
 
     return app
+
+
+
+if __name__ == "__main__":
+    default_positions = {
+        "shoulder_pan.pos": 0,
+        "shoulder_lift.pos": 0,
+        "elbow_flex.pos": 0,
+        'wrist_flex.pos': 0,
+        "wrist_roll.pos": 0,
+        "gripper.pos": 0,
+    }
+
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+    app = create_app(current_positions=default_positions)
+    app.run()
