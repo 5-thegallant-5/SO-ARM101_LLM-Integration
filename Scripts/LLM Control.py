48a9e7d33a9ba2cb63c5369d36d5cf14cb4ba6fe
Stuart Kennedy <stuartkennedy1704@gmail.com>
2025-10-24 16:27:38 +1100

    Cleaned up and added in line documentation for LLM & VLM control

--- Scripts/LLM Control.py
+++ Scripts/LLM Control.py
@@ -1,15 +1,32 @@
+# LLM Control.py: Allows for the llama3.2 LLM model being run through Ollama on the same computer
+# to manipulate the SO-100 Robotic Arm via set positions in accordance to the general instructions that the user provides
+# Author: Stuart Kennedy U3239456
+
+
+# THIRD PARTY APPLICATION NEEDED. needs the Ollama application running the llama3.2 model to work.
+# Download Ollama here: https://ollama.com, Run this command in cmd "Ollama run Llama3.2"
+
 import ollama
 import time
 
 from lerobot.robots.so100_follower import SO100FollowerConfig, SO100Follower
 
+## if Ollama with llama3.2 installed is running on a server on the same network you uncomment this code
+## Allows connection to an Ollama application running on a different computer on the same network
+# client = ollama.Client(
+#   host='http://localhost:11434'
+# )
+
+#Configure the SO-100 Arm will need to change port based on the port being used by the device
 robot_config = SO100FollowerConfig(
     port="COM5",
     id="1",
 )
 
+#the function the LLM can call to change which position the arm is in
 def positions(position: int):
     if position == 1:
+        #puts the arm in the rest position
         pre_rest = {
             'shoulder_pan.pos': -0.5032350826743368,
             'shoulder_lift.pos': -60.932038834951456,
@@ -34,6 +51,7 @@ def positions(position: int):
         return "Robot at the Rest Position"
 
     elif position == 2:
+        #puts the arm in the control position where all joints are set to 0
         action = {
             "shoulder_pan.pos": 0,
             "shoulder_lift.pos": 0,
@@ -49,6 +67,7 @@ def positions(position: int):
         return "Robot at the Control Position"
 
     elif position == 3:
+        #set the arm to be pointing straight up in the air
         action = {
             'shoulder_pan.pos': -2.3005032350826724,
             'shoulder_lift.pos': 4.776699029126206,
@@ -64,6 +83,7 @@ def positions(position: int):
         return "Robot at the Up Position"
 
     elif position == 4:
+        #sets the arm to be pointing directly forward
         action = {
             'shoulder_pan.pos': -1.3659237958303407,
             'shoulder_lift.pos': 75.92233009708738,
@@ -79,6 +99,7 @@ def positions(position: int):
         return "Robot at the Forward Position"
 
     elif position == 5:
+        #sets the end-affector to be in the most left position of the work place
         action = {
             'shoulder_pan.pos': -74.40690150970525,
             'shoulder_lift.pos': 74.99029126213591,
@@ -94,6 +115,7 @@ def positions(position: int):
         return "Robot at the Left Position"
 
     elif position == 6:
+        #set the end-affector to be in the most right position in the workplace
         action = {
             'shoulder_pan.pos': 69.66211358734725,
             'shoulder_lift.pos': 80.73786407766988,
@@ -109,6 +131,7 @@ def positions(position: int):
         return "Robot at the Right Position"
 
     elif position == 7:
+        #opens the gripper at the end-affector
         action = {
             'gripper.pos': 27.6756756756756757
         }
@@ -119,6 +142,7 @@ def positions(position: int):
         return "Gripper open"
 
     elif position == 8:
+        #closes the gripper at the end-affector
         action = {
             'gripper.pos': 0.6756756756756757
         }
@@ -129,18 +153,23 @@ def positions(position: int):
         return "Gripper Closed"
 
     else:
+        #if sent a position outside the scope return "Invalid Position"
         return "Invalid Position"
 
 
+#configers & Connects the SO-100 arm
 robot = SO100Follower(robot_config)
 robot.connect()
 print("Robot Connected")
+#Gets the observation of what position each of the SO-100's joints are in
 print(robot.get_observation())
 
+#details the available functions for the LLM to use
 available_functions = {
   'positions': positions,
 }
 
+#the system prompt sent to the LLM
 sys_prompt = """ You are in control of a robotic arm. This robotic arm has 7 positions.
 Position 1: the Rest position
 Position 2: the Control position
@@ -150,18 +179,20 @@ Position 5: the Left position
 Position 6: the Right position
 Position 7: the Open Gripper position"""
 
+#the user input of the LLM's first task
 user_input = str(input("User: "))
 
+#compiles the message being sent to the LLM
 messages = [
     {"role": "system", "content": sys_prompt},
     {"role": "user", "content": user_input},
 ]
 
-# First response from the bot
+# First response from the LLM. change ollama.chat to client.chat if connecting to Ollama via same network
 response = ollama.chat(model="llama3.2", messages=messages,
 tools=[positions])
 
-#bot calling the functions in tools
+#LLM calling the functions in tools
 for tool in response.message.tool_calls or []:
   function_to_call = available_functions.get(tool.function.name)
   if function_to_call:
@@ -169,20 +200,24 @@ for tool in response.message.tool_calls or []:
   else:
     print('Function not found:', tool.function.name)
 
-# Continue the conversation:
+# Continue giving LLM tasks unless User inputs -1
 while True:
-    user_input = input("You: ")
-    if  user_input == "-1":
-        break  # exit loop on empty input
+    #gets new task from the user
+    user_input = input("User: ")
 
+    if  user_input == "-1":
+        break  # exit loop on input of -1
+
+    #Appends new task to the message being sent to the LLM
     messages.append({"role": "user", "content": user_input})
 
     answer = ""
 
+    #Gets the next response from the LLM. change ollama.chat to client.chat if connecting to Ollama via same network
     response = ollama.chat(model="llama3.2", messages=messages,
                            tools=[positions])
 
-    # bot calling the functions in tools
+    # LLM calling the functions in tools
     for tool in response.message.tool_calls or []:
         function_to_call = available_functions.get(tool.function.name)
         if function_to_call:
@@ -191,73 +226,9 @@ while True:
             answer = 'Function not found: ' + tool.function.name
 
     print("Bot:", answer)
+
+    #appends the LLM's answers to the messages being sent
     messages.append({"role": "assistant", "content": answer})
 
+#disconects the robot
 robot.disconnect()
-
-# prompt = """You are controlling a robotic arm operating in a 10×10 grid workspace.
-#     The grid is made of integer coordinates from (0,0) to (9,9).
-#     The arm starts at position (""" + str(Arm_pos[0]) + """,""" + str(Arm_pos[1]) + """)
-#     and must move to position (""" + str(Target_pos[0]) + """,""" + str(Target_pos[1]) + """).
-#     The arm can only move using these four movements:
-#     forward,
-#     backward,
-#     right,
-#     left,
-#     You can only chose one of the four moves.
-#     What is the next move the arm should take?"""
-#
-# messages = [
-#     {
-#         'role': 'user',
-#         'content': prompt,
-#     },
-# ]
-# response = ollama.chat(model="llama3.2", messages=messages,
-# tools=[sequences])
-#
-# for tool in response.message.tool_calls or []:
-#   function_to_call = available_functions.get(tool.function.name)
-#   if function_to_call:
-#     print('Function output:', function_to_call(**tool.function.arguments))
-#   else:
-#     print('Function not found:', tool.function.name)
-#
-# print(response['message']['content'])
-#
-#
-# while Arm_pos != Target_pos:
-#     if Arm_pos[0] > 10 or Arm_pos[1] > 10:
-#
-#         if Arm_pos[0] > 10:
-#             Arm_pos[0] = 10
-#         elif Arm_pos[1] > 10:
-#             Arm_pos[1] = 10
-#
-#         prompt = "Invalid Move. Moved arm outside of the work space. rest arm to a valid position. "
-#     else:
-#         prompt = ""
-#
-#     prompt = prompt + """The arm position is now at (""" + str(Arm_pos[0]) + """,""" + str(Arm_pos[1]) + """)
-#     and must move to position (""" + str(Target_pos[0]) + """,""" + str(Target_pos[1]) + """).
-#     The arm can only move using these four movements:
-#     forward,
-#     backward,
-#     right,
-#     left,
-#     You can only chose 1 of the four moves."""
-#
-#     response = ollama.chat(model="llama3.2", messages= [
-#         {
-#             'role': 'user',
-#             'content': prompt,
-#         },
-#     ], tools=[movement])
-#     for tool in response.message.tool_calls or []:
-#         function_to_call = available_functions.get(tool.function.name)
-#         if function_to_call:
-#             print('Function output:', function_to_call(**tool.function.arguments))
-#         else:
-#             print('Function not found:', tool.function.name)
-#
-#     print(Arm_pos, Target_pos)
--- Scripts/VLM_Control.py
+++ Scripts/VLM_Control.py
@@ -1,3 +1,12 @@
+# VLM_Control.py: Allows for the llama3.2-vision VLM model being run through Ollama on the same computer
+# to manipulate the SO-100 Robotic Arm via joint manipulation and visual feedback
+# in accordance to the general instructions that the user provides
+# Author: Stuart Kennedy U3239456
+
+
+# THIRD PARTY APPLICATION NEEDED. needs the Ollama application running the llama3.2-vision model to work.
+# Download Ollama here: https://ollama.com, Run this command in cmd "Ollama run Llama3.2-vision"
+
 import json
 import ollama
 import time
@@ -7,13 +16,16 @@ from pydantic import BaseModel, Field
 from lerobot.robots.so100_follower import SO100FollowerConfig, SO100Follower
 from pathlib import Path
 
+# Configure the SO-100 Arm will need to change port based on the port being used by the device
 robot_config = SO100FollowerConfig(
     port="COM5",
     id="1",
 )
 
+#sets the so-100's webcam to cap. will need to change the int in VideoCapture() to be the so100's webcam
 cap = cv2.VideoCapture(2)
 
+#Json Schema that will format the output of the VLM as a dictionary that send.action() can read
 class Action(BaseModel):
     shoulder_pan_pos: float = Field(..., alias='shoulder_pan.pos', description="Rotation angle of the shoulder pan joint.")
     shoulder_lift_pos: float = Field(..., alias='shoulder_lift.pos', description="Lift angle of the shoulder joint.")
@@ -23,6 +35,7 @@ class Action(BaseModel):
     gripper_pos: float = Field(..., alias='gripper.pos', description="Opening or closing position of the gripper.")
 
 
+#function that moves the SOO-100 arm based on the dictionary that it is given
 def movement(action: dict):
 
     robot.send_action(action)
@@ -31,18 +44,15 @@ def movement(action: dict):
     return action
 
 
-
+#configures and connects the SO-100 arm to the computer
 robot = SO100Follower(robot_config)
 robot.connect()
 print("Robot Connected")
+#Get the current joint position of the SO-100 arm
 current_pos = robot.get_observation()
-
 print(current_pos)
 
-available_functions = {
-  'movement': movement,
-}
-
+#function that will put the SO-100 arm into the rest position
 def rest():
     pre_rest = {
         'shoulder_pan.pos': -0.5032350826743368,
@@ -67,10 +77,12 @@ def rest():
 
     return "Goodbye"
 
+#function that sends a message to the VLM and outputs the response
 def vlm_response(message):
 
     response = ollama.chat(model="llama3.2-vision", messages=message, format=Action.model_json_schema())
 
+    #deletes the Frame image from the webcam once the image has been sent to the VLM
     try:
         os.remove(frame_path)
     except FileNotFoundError:
@@ -82,7 +94,7 @@ def vlm_response(message):
 
     return response
 
-
+#The System prompt to the VLM
 sys_prompt = """You are in control of a robotic arm. The arm has six joints.
 You will be given an image of the arm’s current workspace and a generalized instruction describing the desired task.
 
@@ -103,26 +115,33 @@ Your job is to determine the *target joint positions* required for the arm to co
 - gripper is the opening or closing position of the gripper.
 """
 
-
+#Gets the first task from the user
 user_input = str(input("User: "))
 
+#obtains a frame from the webcam and saves it to 'frame_cache/frame.jpeg'
 ret, frame = cap.read()
 frame_path = "frame_cache/frame.jpeg"
 cv2.imwrite(frame_path, frame)
 img_path = Path(frame_path).read_bytes()
 
+# the message sent to the VLM including the image of the frame from the webcam
 message = [
     {"role": "system", "content": sys_prompt},
     {"role": "user", "content": user_input, "images": [img_path]}
 ]
 
+#gets the VLM response from the vlm_response function
 vlm_content = vlm_response(message).message.content
 
+#prints the vlm_response
 print(vlm_content)
+#converts the vlm_response into a dictionary
 vlm_dict = json.loads(vlm_content)
 print(vlm_dict)
+#moves the arm in accordance to the vlm_response
 movement(vlm_dict)
 
+#takes tasks from the User until the user inputs -1
 while True:
     user_input = input("You: ")
     if user_input == "-1":
@@ -130,16 +149,18 @@ while True:
         robot.disconnect()
         break
 
+    # obtains a frame from the webcam and saves it to 'frame_cache/frame.jpeg'
     ret, frame = cap.read()
     frame_path = "frame_cache/frame.png"
     cv2.imwrite(frame_path, frame)
     img_path = Path(frame_path).read_bytes()
 
+    #compiles the next message being sent to the VLM
     message = [
         {"role": "system", "content": sys_prompt},
         {"role": "user", "content": user_input, "images": img_path}
     ]
 
-
+    #Get the response from the vlm
     answer = vlm_response(message).message.content
     print("Bot:", answer)
